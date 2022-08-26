d8c35fcca76c44da9755d2f3aa99e3deca987af6
Karolina Lipnitskaite <lipnitskaite@gmail.com>
2022-08-26 16:59:46 +0200

    feat: set the project structure

---
+++ src/components/App/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          The main page
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
