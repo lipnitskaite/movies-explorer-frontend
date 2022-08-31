e28242050b51a0707c0cc6ddbc47c0d9f65ca4de
Karolina Lipnitskaite <lipnitskaite@gmail.com>
2022-08-31 14:48:05 +0200

    feat: add App component + styles, add variables

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,20 @@
-import './App.css';
+import '../App/App.css';
+
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import PageNotFound from '../PageNotFound/PageNotFound';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          The main page
-        </p>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="app">
+        <Switch>
+          <Route path='*'>
+            <PageNotFound />
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
