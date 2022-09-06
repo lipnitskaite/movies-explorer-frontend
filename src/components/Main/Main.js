514bcb45f907574b1bdc8f32f304510830518916
Karolina Lipnitskaite <lipnitskaite@gmail.com>
2022-09-06 14:13:19 +0200

    feat: handle Techs layout for 320px

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 import Promo from '../Promo/Promo'
 import AboutProject from '../AboutProject/AboutProject'
+import Techs from '../Techs/Techs'
 
 function Main() {
   return (
     <main className="content">
       <Promo />
       <AboutProject />
+      <Techs />
     </main>
   );
 }
