144c8e094d21734f93c9a6f40419b36449c8c4ad
Karolina Lipnitskaite <lipnitskaite@gmail.com>
2022-09-16 18:02:48 +0200

    feat: handle Filter Checkbox layout for 320px

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -2,15 +2,18 @@ import '../SearchForm/SearchForm.css';
 
 import React from 'react';
 
-function AboutMe() {
+import FilterCheckbox from '../FilterCheckbox/FilterCheckbox';
+
+function SearchForm() {
   return (
     <section className='search-form'>
       <form className='search-form__container'>
         <input className='search-form__input' placeholder='Фильм' type='search' required></input>
         <button className='search-form__button' type='submit'></button>
       </form>
+      <FilterCheckbox />
     </section>
   );
 }
 
-export default AboutMe;
+export default SearchForm;
