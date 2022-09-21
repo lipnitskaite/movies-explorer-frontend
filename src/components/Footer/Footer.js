a84fb3bb2eea37342c55d5646697011166db1c61
Karolina Lipnitskaite <lipnitskaite@gmail.com>
2022-09-21 12:32:52 +0200

    feat: adapt Footer for 768px

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,15 +6,16 @@ function Footer() {
   return (
     <footer className="footer">
       <p className='footer__text'>Учебный проект Яндекс.Практикум х BeatFilm.</p>
-      <div className='footer__divider'></div>
-      <nav>
-        <ul className='footer__nav'>
-          <li className='footer__nav_item'><a className='footer__link' href='https://practicum.yandex.ru/' target='_blank' rel='noreferrer'>Яндекс.Практикум</a></li>
-          <li className='footer__nav_item'><a className='footer__link' href='https://github.com/lipnitskaite' target='_blank' rel="noreferrer">Github</a></li>
-          <li className='footer__nav_item'><a className='footer__link' href='https://www.facebook.com/yandex.practicum/' target='_blank' rel='noreferrer'>Facebook</a></li>
-        </ul> 
-      </nav>
-      <p className='footer__text'>©2022</p>
+      <div className='footer__container'>
+        <nav>
+          <ul className='footer__nav'>
+            <li className='footer__nav_item'><a className='footer__link' href='https://practicum.yandex.ru/' target='_blank' rel='noreferrer'>Яндекс.Практикум</a></li>
+            <li className='footer__nav_item'><a className='footer__link' href='https://github.com/lipnitskaite' target='_blank' rel="noreferrer">Github</a></li>
+            <li className='footer__nav_item'><a className='footer__link' href='https://www.facebook.com/yandex.practicum/' target='_blank' rel='noreferrer'>Facebook</a></li>
+          </ul> 
+        </nav>
+        <p className='footer__text footer__text_type_copyright'>© 2022</p>
+      </div>
     </footer>
   );
 }
