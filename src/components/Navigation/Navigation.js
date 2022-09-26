863e80416dff49921e222f8bd597b3ce54b07af2
Karolina Lipnitskaite <lipnitskaite@gmail.com>
2022-09-26 16:19:45 +0200

    feat: adapt Navigation layout for 768px

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -5,52 +5,55 @@ import { NavLink, Link } from 'react-router-dom';
         
 function Navigation({ isOpen, onClose }) {
   return (
-    <div className={`menu ${isOpen && 'menu_opened'}`}>
-      <button 
-        className='menu__close-button'
-        type='button'
-        onClick={onClose}
-      ></button>
-      <nav className='menu__nav'>
-        <ul className='menu__nav-list'>
-          <li className='menu__nav-item'>
-            <NavLink 
-              className='menu__link menu__link_type_nav'
-              to='/'
-              exact={true}
-              onClick={onClose}
-            >
-              Главная
-            </NavLink>
-          </li>
-          <li className='menu__nav-item'>
-            <NavLink
-              className='menu__link menu__link_type_nav'
-              to='/movies'
-              onClick={onClose}
-            >
-              Фильмы
-            </NavLink>
-          </li>
-          <li className='menu__nav-item'>
-            <NavLink 
-              className='menu__link menu__link_type_nav'
-              to={'/saved-movies'}
-              onClick={onClose}
-            >
-              Сохранённые фильмы
-            </NavLink>
-          </li>
-        </ul>  
-      </nav>
-      <Link
-        className='menu__link menu__link_type_main'
-        to={'/profile'}
-        onClick={onClose}
-      >
-        Аккаунт
-      </Link>
-    </div> 
+    <section className={`menu ${isOpen && 'menu_opened'}`}>
+      <div className="menu__overlay" onClick={onClose}></div>
+      <div className='menu__container'>
+        <button 
+          className='menu__close-button'
+          type='button'
+          onClick={onClose}
+        ></button>
+        <nav className='menu__nav'>
+          <ul className='menu__nav-list'>
+            <li className='menu__nav-item'>
+              <NavLink 
+                className='menu__link menu__link_type_nav'
+                to='/'
+                exact={true}
+                onClick={onClose}
+              >
+                Главная
+              </NavLink>
+            </li>
+            <li className='menu__nav-item'>
+              <NavLink
+                className='menu__link menu__link_type_nav'
+                to='/movies'
+                onClick={onClose}
+              >
+                Фильмы
+              </NavLink>
+            </li>
+            <li className='menu__nav-item'>
+              <NavLink 
+                className='menu__link menu__link_type_nav'
+                to={'/saved-movies'}
+                onClick={onClose}
+              >
+                Сохранённые фильмы
+              </NavLink>
+            </li>
+          </ul>  
+        </nav>
+        <Link
+          className='menu__link menu__link_type_main'
+          to={'/profile'}
+          onClick={onClose}
+        >
+          Аккаунт
+        </Link>
+      </div>
+    </section> 
   );
 }
 
