f8c8f3d2cba58af7db340f22dd0ffaade1f70d95
Karolina Lipnitskaite <lipnitskaite@gmail.com>
2022-10-28 12:22:35 +0200

    feat: handle Load More button

--- src/components/Movies/MoviesCardList/MoviesCard/MoviesCard.js
+++ src/components/Movies/MoviesCardList/MoviesCard/MoviesCard.js
@@ -4,7 +4,15 @@ import React from 'react';
 
 import { transformMovieDuration } from '../../../../utils/utils';
 
-function MoviesCard({ card, isSavedCard }) {
+function MoviesCard({ card, isSaved }) {
+  const cardSaveButtonClassName = (
+    `card__like-button ${isSaved ? 'movie-card__toggle movie-card__toggle_disabled' : 'movie-card__toggle'}`
+  );
+
+  const cardDeleteButtonClassName = (
+    `card__like-button ${isSaved ? 'movie-card__button' : 'movie-card__button movie-card__button_disabled'}`
+  );
+
   return (
     <article className='movie-card movies__cards'>
       <a href={card.trailerLink} target='_blank' rel='noreferrer'>
@@ -12,8 +20,8 @@ function MoviesCard({ card, isSavedCard }) {
       </a>
       <div className='movie-card__container'>
         <h4 className='movie-card__title'>{card.nameRU}</h4>
-        <input className={isSavedCard ? 'movie-card__toggle movie-card__toggle_disabled' : 'movie-card__toggle'} type='checkbox'></input>
-        <button className={isSavedCard ? 'movie-card__button' : 'movie-card__button movie-card__button_disabled'} type='button'></button>
+        <input className={cardSaveButtonClassName} type='checkbox'></input>
+        <button className={cardDeleteButtonClassName} type='button'></button>
       </div>
       <p className='movie-card__text'>{transformMovieDuration(card.duration)}</p>
     </article>
--- src/components/Movies/MoviesCardList/MoviesCardList.js
+++ src/components/Movies/MoviesCardList/MoviesCardList.js
@@ -1,25 +1,46 @@
 import '../MoviesCardList/MoviesCardList.css';
 
-import React from 'react';
+import React, { useState } from 'react';
 
 import MoviesCard from '../MoviesCardList/MoviesCard/MoviesCard';
 
 function MoviesCardList({ movies, isSavedList}) {
   const savedCard = isSavedList;
-  
+  const width = window.innerWidth;
+  let initialImagePerRow;
+  let nextImagePerRow;
+
+  if (width <= 767) {
+    initialImagePerRow = 5;
+    nextImagePerRow = 2;
+  } else if (width >= 768 && width <= 1279) {
+    initialImagePerRow = 8;
+    nextImagePerRow = 2;
+  } else if (width >= 1280) {
+    initialImagePerRow = 12;
+    nextImagePerRow = 4;
+  } 
+
+  const [row, setRow] = useState(initialImagePerRow);
+
+  function loadMoreImage() {
+    setRow(row + nextImagePerRow);
+  }
+
   return (
     <section className='movies'>
       <div className='movies__container'>
-        {movies.map((card) => (
+        {movies?.slice(0, row)?.map((card) => (
           <MoviesCard
             card={card}
-            isSavedCard={savedCard}
+            isSaved={savedCard}
           />
         ))}
       </div>
       <button
         className={isSavedList ? 'movies__button movies__button_disabled' : 'movies__button'}
         type='button'
+        onClick={loadMoreImage}
       >Ещё</button>
     </section>
   );
