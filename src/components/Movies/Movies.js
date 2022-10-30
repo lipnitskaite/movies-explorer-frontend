fbca4ebe32d2049d8b566c84a83cc79bcf5ae592
Karolina Lipnitskaite <lipnitskaite@gmail.com>
2022-10-30 21:09:44 +0100

    feat: dynamic movies search

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 
+import '../Movies/Movies.css';
+
 import { moviesApi } from '../../utils/constants';
 import SearchForm from '../SearchForm/SearchForm';
 import Preloader from '../Preloader/Preloader';
@@ -8,25 +10,47 @@ import MoviesCardList from '../Movies/MoviesCardList/MoviesCardList';
 function Movies() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [submitError, setSubmitError] = useState({});
+  const [isValid, setIsValid] = useState(true);
+
+  function handleChange(event) {
+    const target = event.target;
+    const name = target.name;
+    const value = target.value;
+
+    setSearchTerm(value);
+    setSubmitError({ ...submitError, [name]: target.validationMessage });
+    setIsValid(target.closest('form').checkValidity());
+  };
 
   useEffect(() => {
     moviesApi.getMovies()
       .then((movies) => {
-        setMovies(movies);
+        const results = movies.filter(movie => movie.nameRU.toLowerCase().includes(searchTerm));
+        setMovies(results);
         setIsLoading(false);
       })
-      .catch(err => console.log(err)); //обработать ошибку
-  }, [])
+      .catch(err => setSubmitError(err))
+  }, [searchTerm]);
 
   return (
     <main className="content">
-      <SearchForm />
+      <SearchForm
+        searchTerm={searchTerm}
+        setSearchTerm={setSearchTerm}
+        setIsValid={setIsValid}
+        handleChange={handleChange}
+        setSubmitError={setSubmitError}
+      />
       <Preloader 
         isLoading={isLoading}
       />
+      <span className={`movies-form__error ${!isValid && 'movies-form__error_active'}`}>Нужно ввести ключевое слово</span>
       <MoviesCardList
         isLoading={isLoading}
         movies={movies}
+        isError={!isValid}
       />
     </main>
   );
--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,15 +1,22 @@
 import '../SearchForm/SearchForm.css';
 
 import React from 'react';
-
 import FilterCheckbox from '../FilterCheckbox/FilterCheckbox';
 
-function SearchForm() {
+function SearchForm({ searchTerm, handleChange }) {
+
   return (
     <section className='search-form'>
       <div className='search-form__container'>
-        <form className='search-form__form'>
-          <input className='search-form__input' placeholder='Фильм' type='search' required></input>
+        <form className='search-form__form' name='search-form' noValidate>
+          <input 
+            className='search-form__input'
+            placeholder='Фильм'
+            type='search'
+            value={searchTerm || ''}
+            onChange={handleChange}
+            required
+          ></input>
           <button className='search-form__button' type='submit'></button>
         </form>
         <FilterCheckbox />
