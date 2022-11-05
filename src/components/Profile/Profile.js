b17cd87c0f6d6c59fffd5067dd0585724d960e7f
Karolina Lipnitskaite <lipnitskaite@gmail.com>
2022-11-05 17:46:34 +0100

    fix: profile layout

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -4,8 +4,8 @@ import React, { useContext, useEffect, useState } from 'react';
 import UseFormValidation from '../Validation/Validation';
 import { CurrentUserContext } from '../../contexts/CurrentUserContext';
 
-function Profile({ isLoading, submitError }) {
-  const { handleChange, errors, isValid } = UseFormValidation();
+function Profile({ isLoading, submitError, handleUpdateUserInfo, setSubmitError }) {
+  const { values, resetForm, handleChange, errors, isValid } = UseFormValidation();
 
   const currentUser = useContext(CurrentUserContext);
 
@@ -20,35 +20,55 @@ function Profile({ isLoading, submitError }) {
     }
   }, [currentUser]);
 
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    handleUpdateUserInfo(values)
+    .catch(err => setSubmitError(err))
+    resetForm();
+  }
+
   return (
     <section className={`profile ${isLoading && 'profile_hidden'}`}>
       <h2 className='profile__title'>{`Привет, ${name}!`}</h2>
-      <form className='profile__form' name='user-update' noValidate>
+      <form className='profile__form' name='user-update' noValidate onSubmit={handleSubmit}>
         <fieldset className='profile__form-container profile__form-container_type_profile-info'>
-          <label className='profile__form-label'>Имя</label>
-          <input 
-            className={`profile__form-input ${errors.name && 'profile__form-input_type_error'}`}
-            onChange={handleChange}
-            type='text'
-            id='name'
-            name='name'
-            value={name || ''}
-            required
-            minLength={2}
-            maxLength={30}
-            pattern='[A-Za-zа-яА-ЯёЁ\-\s]+'
-          ></input>
+          <div className='profile__form-row'>
+            <label className='profile__form-label'>Имя</label>
+            <input 
+              className={`profile__form-input ${errors.name && 'profile__form-input_type_error'}`}
+              onChange={handleChange}
+              type='text'
+              id='name'
+              name='name'
+              value={
+                isEditing
+                ? values.name
+                : name || ''
+              }
+              required
+              minLength={2}
+              maxLength={30}
+              pattern='[A-Za-zа-яА-ЯёЁ\-\s]+'
+            ></input>
+          </div>
           <span className={`form__input-error ${errors.name && 'form__input-error_active'}`}>{errors.name || ''}</span>
-          <label className='profile__form-label'>E-mail</label>
-          <input 
-            className={`profile__form-input ${errors.email && 'profile__form-input_type_error'}`}
-            onChange={handleChange}
-            type='email'
-            id='email'
-            name='email'
-            value={email || ''}
-            required
-          ></input>
+          <div className='profile__form-row'>
+            <label className='profile__form-label'>E-mail</label>
+            <input 
+              className={`profile__form-input ${errors.email && 'profile__form-input_type_error'}`}
+              onChange={handleChange}
+              type='email'
+              id='email'
+              name='email'
+              value={
+                isEditing
+                ? values.email
+                : email || ''
+              }
+              required
+            ></input>
+          </div>
           <span className={`form__input-error ${errors.email && 'form__input-error_active'}`}>{errors.email || ''}</span>
         </fieldset>
         <div className='profile__form-container profile__form-container_type_buttons'>
